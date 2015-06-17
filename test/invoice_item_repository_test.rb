38a6f826cbf1e11fa9f87976633ad250962ef059
Marla Brizel <marlabrizel@gmail.com>
2015-06-17 16:42:17 -0600

    refactor to use new parser workflow and fix failing tests

--- test/invoice_item_repository_test.rb
+++ test/invoice_item_repository_test.rb
@@ -1,256 +1,113 @@
 require_relative 'test_helper'
 require './lib/invoice_item_repository'
+require './lib/sales_engine'
 
 class InvoiceItemRepositoryTest < Minitest::Test
 
-  def test_it_has_an_invoice_item_repo
-    data = ["invoice_item 1"]
-    sales_engine = "engine"
-    repo = InvoiceItemRepository.new(data, sales_engine)
+  attr_reader :invoice_item_repository
 
-    assert repo
+  def setup
+    engine = SalesEngine.new(fixtures_directory)
+    engine.startup
+    @invoice_item_repository = engine.invoice_item_repository
   end
 
-  def test_it_finds_all_invoice_items
-    data = ["1", "2", "3", "4", "5", "6", "7"]
-    sales_engine = "engine"
-    repo = InvoiceItemRepository.new(data, sales_engine)
-    expected = ["1", "2", "3", "4", "5", "6", "7"]
-    output = repo.all
-
-    assert_equal expected, output
-  end
-
-  def test_it_finds_a_random_invoice_item
-    data = ["1", "2", "3", "4", "5", "6", "7"]
-    sales_engine = "engine"
-    repo = InvoiceItemRepository.new(data, sales_engine)
-    output1 = repo.random
-    output2 = repo.random
-
-    refute_equal output1, output2
-  end
-
-  def test_invoice_item_repository_holds_invoice_item_instances
-    sales_engine = "Placeholder"
-    invoice_item_repository = "Placeholder"
-    input_csv = fixture_path('invoice_items_fixture.csv')
-    data = InvoiceItemParser.new(input_csv, invoice_item_repository).load_csv
-    repo = InvoiceItemRepository.new(data, sales_engine)
-
-    first_invoice_item = repo.invoice_items[0]
-
-    assert_equal InvoiceItem, first_invoice_item.class
+  def test_it_finds_a_single_invoice_item
+    assert_equal 539, invoice_item_repository.find_by_id(1).item_id
   end
 
   def test_it_finds_invoice_items_by_id
-    sales_engine = "Placeholder"
-    invoice_item_repository = "Placeholder"
-    input_csv = fixture_path('invoice_items_fixture.csv')
-    data = InvoiceItemParser.new(input_csv, invoice_item_repository).load_csv
-    repo = InvoiceItemRepository.new(data, sales_engine)
-    expected = 1
-
-    output = repo.find_by_id(1)
-
-    assert_equal expected, output.invoice_id
+    result = invoice_item_repository.find_by_id(1)
+    assert_equal 539, result.item_id
   end
 
   def test_it_finds_invoice_items_by_item_id
-    sales_engine = "Placeholder"
-    invoice_item_repository = "Placeholder"
-    input_csv = fixture_path('invoice_items_fixture.csv')
-    data = InvoiceItemParser.new(input_csv, invoice_item_repository).load_csv
-    repo = InvoiceItemRepository.new(data, sales_engine)
-    expected = 1
-
-    output = repo.find_by_item_id(539)
-
-    assert_equal expected, output.invoice_id
+    result = invoice_item_repository.find_by_item_id(539)
+    assert_equal 1, result.id
   end
 
   def test_it_finds_invoice_items_by_invoice_id
-    sales_engine = "Placeholder"
-    invoice_item_repository = "Placeholder"
-    input_csv = fixture_path('invoice_items_fixture.csv')
-    data = InvoiceItemParser.new(input_csv, invoice_item_repository).load_csv
-    repo = InvoiceItemRepository.new(data, sales_engine)
-    expected = 539
-
-    output = repo.find_by_invoice_id(1)
-
-    assert_equal expected, output.item_id
+    result = invoice_item_repository.find_by_item_id(539)
+    assert_equal 1, result.id
   end
 
   def test_it_finds_invoice_items_by_quantity
-    sales_engine = "Placeholder"
-    invoice_item_repository = "Placeholder"
-    input_csv = fixture_path('invoice_items_fixture.csv')
-    data = InvoiceItemParser.new(input_csv, invoice_item_repository).load_csv
-    repo = InvoiceItemRepository.new(data, sales_engine)
-    expected = 1
-
-    output = repo.find_by_quantity(5)
-
-    assert_equal expected, output.invoice_id
+    result = invoice_item_repository.find_by_quantity(5)
+    assert_equal 1, result.id
   end
 
   def test_it_finds_invoice_items_by_unit_price
-    sales_engine = "Placeholder"
-    invoice_item_repository = "Placeholder"
-    input_csv = fixture_path('invoice_items_fixture.csv')
-    data = InvoiceItemParser.new(input_csv, invoice_item_repository).load_csv
-    repo = InvoiceItemRepository.new(data, sales_engine)
-    expected = 1
-
-    output = repo.find_by_unit_price(136.35)
-
-    assert_equal expected, output.invoice_id
+    result = invoice_item_repository.find_by_unit_price(136.35)
+    assert_equal 1, result.id
   end
 
   def test_it_finds_invoice_items_by_created_at
-    sales_engine = "Placeholder"
-    invoice_item_repository = "Placeholder"
-    input_csv = fixture_path('invoice_items_fixture.csv')
-    data = InvoiceItemParser.new(input_csv, invoice_item_repository).load_csv
-    repo = InvoiceItemRepository.new(data, sales_engine)
-    expected = 1
-
-    output = repo.find_by_created_at("2012-03-27 14:54:09 UTC")
-
-    assert_equal expected, output.invoice_id
+    result = invoice_item_repository.find_by_created_at("2012-03-27 14:54:09 UTC")
+    assert_equal 1, result.id
   end
 
   def test_it_finds_invoice_items_by_updated_at
-    sales_engine = "Placeholder"
-    invoice_item_repository = "Placeholder"
-    input_csv = fixture_path('invoice_items_fixture.csv')
-    data = InvoiceItemParser.new(input_csv, invoice_item_repository).load_csv
-    repo = InvoiceItemRepository.new(data, sales_engine)
-    expected = 1
-
-    output = repo.find_by_updated_at("2012-03-27 14:54:09 UTC")
-
-    assert_equal expected, output.invoice_id
+    result = invoice_item_repository.find_by_updated_at("2012-03-27 14:54:09 UTC")
+    assert_equal 1, result.id
   end
 
   def test_it_finds_all_invoice_items_by_id
-    sales_engine = "Placeholder"
-    invoice_item_repository = "Placeholder"
-    input_csv = fixture_path('invoice_items_fixture.csv')
-    data = InvoiceItemParser.new(input_csv, invoice_item_repository).load_csv
-    repo = InvoiceItemRepository.new(data, sales_engine)
-    expected_1 = 1
-    expected_2 = []
-
-    output_1 = repo.find_all_by_id(1)
-    output_2 = repo.find_all_by_id(11)
-
-    assert_equal expected_1, output_1[0].invoice_id
-    assert_equal expected_2, output_2
+    result1 = invoice_item_repository.find_all_by_id(1)
+    result2 = invoice_item_repository.find_all_by_id(11)
+    assert_equal Array, result1.class
+    assert_equal 1, result1.length
+    assert_equal 0, result2.length
   end
 
   def test_it_finds_all_invoice_items_by_item_id
-    sales_engine = "Placeholder"
-    invoice_item_repository = "Placeholder"
-    input_csv = fixture_path('invoice_items_fixture.csv')
-    data = InvoiceItemParser.new(input_csv, invoice_item_repository).load_csv
-    repo = InvoiceItemRepository.new(data, sales_engine)
-    expected_1 = 1
-    expected_2 = []
-
-    output_1 = repo.find_all_by_item_id(539)
-    output_2 = repo.find_all_by_item_id(400)
-
-    assert_equal expected_1, output_1[0].id
-    assert_equal expected_2, output_2
+    result1 = invoice_item_repository.find_all_by_item_id(539)
+    result2 = invoice_item_repository.find_all_by_item_id(540)
+    assert_equal Array, result1.class
+    assert_equal 1, result1.length
+    assert_equal 0, result2.length
   end
 
   def test_find_all_invoice_items_by_invoice_id
-    sales_engine = "Placeholder"
-    invoice_item_repository = "Placeholder"
-    input_csv = fixture_path('invoice_items_fixture.csv')
-    data = InvoiceItemParser.new(input_csv, invoice_item_repository).load_csv
-    repo = InvoiceItemRepository.new(data, sales_engine)
-    expected_1 = 1
-    expected_2 = []
-
-    output_1 = repo.find_all_by_invoice_id(1)
-    output_2 = repo.find_all_by_invoice_id(7)
-
-    assert_equal expected_1, output_1[0].id
-    assert_equal expected_2, output_2
+    result1 = invoice_item_repository.find_all_by_invoice_id(1)
+    result2 = invoice_item_repository.find_all_by_invoice_id(7)
+    assert_equal Array, result1.class
+    assert_equal 8, result1.length
+    assert_equal 0, result2.length
   end
 
   def test_find_all_invoice_items_by_quantity
-    sales_engine = "Placeholder"
-    invoice_item_repository = "Placeholder"
-    input_csv = fixture_path('invoice_items_fixture.csv')
-    data = InvoiceItemParser.new(input_csv, invoice_item_repository).load_csv
-    repo = InvoiceItemRepository.new(data, sales_engine)
-    expected_1 = 1
-    expected_2 = []
-
-    output_1 = repo.find_all_by_quantity(5)
-    output_2 = repo.find_all_by_quantity(16)
-
-    assert_equal expected_1, output_1[0].id
-    assert_equal expected_2, output_2
+    result1 = invoice_item_repository.find_all_by_quantity(5)
+    result2 = invoice_item_repository.find_all_by_quantity(8657)
+    assert_equal Array, result1.class
+    assert_equal 2, result1.length
+    assert_equal 0, result2.length
   end
 
   def test_find_all_invoice_items_by_unit_price
-    sales_engine = "Placeholder"
-    invoice_item_repository = "Placeholder"
-    input_csv = fixture_path('invoice_items_fixture.csv')
-    data = InvoiceItemParser.new(input_csv, invoice_item_repository).load_csv
-    repo = InvoiceItemRepository.new(data, sales_engine)
-    expected_1 = 4
-    expected_2 = []
-
-    output_1 = repo.find_all_by_unit_price(21.96)
-    output_2 = repo.find_all_by_unit_price(85.99)
-
-    assert_equal expected_1, output_1[0].id
-    assert_equal expected_2, output_2
+    result1 = invoice_item_repository.find_all_by_unit_price(21.96)
+    result2 = invoice_item_repository.find_all_by_unit_price(86.57)
+    assert_equal Array, result1.class
+    assert_equal 1, result1.length
+    assert_equal 0, result2.length
   end
 
   def test_find_all_invoice_items_by_created_at
-    sales_engine = "Placeholder"
-    invoice_item_repository = "Placeholder"
-    input_csv = fixture_path('invoice_items_fixture.csv')
-    data = InvoiceItemParser.new(input_csv, invoice_item_repository).load_csv
-    repo = InvoiceItemRepository.new(data, sales_engine)
-    expected_1 = 1
-    expected_2 = []
-
-    output_1 = repo.find_all_by_created_at("2012-03-27 14:54:09 UTC")
-    output_2 = repo.find_all_by_created_at("2016-03-27 14:54:09 UTC")
-
-    assert_equal expected_1, output_1[0].id
-    assert_equal expected_2, output_2
+    result1 = invoice_item_repository.find_all_by_created_at("2012-03-27 14:54:09 UTC")
+    result2 = invoice_item_repository.find_all_by_created_at("2016-03-27 14:54:09 UTC")
+    assert_equal Array, result1.class
+    assert_equal 10, result1.length
+    assert_equal 0, result2.length
   end
 
   def test_find_all_invoice_items_by_updated_at
-    sales_engine = "Placeholder"
-    invoice_item_repository = "Placeholder"
-    input_csv = fixture_path('invoice_items_fixture.csv')
-    data = InvoiceItemParser.new(input_csv, invoice_item_repository).load_csv
-    repo = InvoiceItemRepository.new(data, sales_engine)
-    expected_1 = 1
-    expected_2 = []
-
-    output_1 = repo.find_all_by_created_at("2012-03-27 14:54:09 UTC")
-    output_2 = repo.find_all_by_created_at("2016-03-27 14:54:09 UTC")
-
-    assert_equal expected_1, output_1[0].id
-    assert_equal expected_2, output_2
+    result1 = invoice_item_repository.find_all_by_updated_at("2012-03-27 14:54:09 UTC")
+    result2 = invoice_item_repository.find_all_by_updated_at("2016-03-27 14:54:09 UTC")
+    assert_equal Array, result1.class
+    assert_equal 10, result1.length
+    assert_equal 0, result2.length
   end
 
   private
-
-  def fixture_path(file_name)
-    File.join(fixtures_directory, file_name)
-  end
 
   def fixtures_directory
     File.expand_path('../../data/fixtures', __FILE__)
