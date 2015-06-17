79472770ba80fb76e71cfbc12a0237ec3cdc76d6
Marla Brizel <marlabrizel@gmail.com>
2015-06-17 09:58:49 -0600

    change strings to integers for relevant data

--- lib/invoice_item.rb
+++ lib/invoice_item.rb
@@ -11,10 +11,10 @@ class InvoiceItem
               :repository
 
   def initialize(data, repository)
-    @id = data[:id]
-    @item_id = data[:item_id]
-    @invoice_id = data[:invoice_id]
-    @quantity = data[:quantity]
+    @id = data[:id].to_i
+    @item_id = data[:item_id].to_i
+    @invoice_id = data[:invoice_id].to_i
+    @quantity = data[:quantity].to_i
     @unit_price = (BigDecimal.new(data[:unit_price])/100).to_f
     @created_at = data[:created_at]
     @updated_at = data[:updated_at]
