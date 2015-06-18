6f0b0131609f9ddd4523e7507e4a99cdfdcfd346
Marla Brizel <marlabrizel@gmail.com>
2015-06-17 20:23:27 -0600

    refactor to take new parse workflow

--- test/customer_repository_test.rb
+++ test/customer_repository_test.rb
@@ -1,188 +1,99 @@
 require_relative 'test_helper'
 require './lib/customer_repository'
+require './lib/sales_engine'
 
 class CustomerRepositoryTest < Minitest::Test
-  def test_it_has_a_sales_engine
-    data = [" merchant 1"]
-    sales_engine = "engine"
-    repo = CustomerRepository.new(data, sales_engine)
 
-    assert_equal "engine", repo.sales_engine
+  attr_reader :customer_repository
+
+  def setup
+    engine = SalesEngine.new(fixtures_directory)
+    engine.startup
+    @customer_repository = engine.customer_repository
   end
 
-  def test_it_finds_all_customers
-    data = ["John", "Paul", "George", "Ringo"]
-    sales_engine = "engine"
-    repo = CustomerRepository.new(data, sales_engine)
-
-    output = repo.all
-
-    assert_equal ["John", "Paul", "George", "Ringo"], output
+  def test_it_find_all_customers
+    assert_equal 10, customer_repository.all.count
   end
 
   def test_it_has_a_random_method
-    data = ["John", "Paul", "George", "Ringo"]
-    sales_engine = "engine"
-    repo = CustomerRepository.new(data, sales_engine)
-
-    assert repo.respond_to?(:random)
-  end
-
-  def test_customer_repository_holds_customer_instances
-    sales_engine = "Placeholder"
-    merchant_repository = "Placeholder"
-    input_csv = fixture_path('customers_fixture.csv')
-    data = CustomerParser.new(input_csv, merchant_repository).load_csv
-    repo = CustomerRepository.new(data, sales_engine)
-
-    first_customer = repo.customers[0]
-
-    assert_equal Customer, first_customer.class
+    assert customer_repository.respond_to?(:random)
   end
 
   def test_it_finds_customers_by_id
-    sales_engine = "Placeholder"
-    merchant_repository = "Placeholder"
-    input_csv = fixture_path('customers_fixture.csv')
-    data = CustomerParser.new(input_csv, merchant_repository).load_csv
-    repo = CustomerRepository.new(data, sales_engine)
-
-    output = repo.find_by_id(1)
-
-    assert_equal "Joey", output.first_name
+    result = customer_repository.find_by_id(1)
+    assert_equal "Joey", result.first_name
   end
 
   def test_it_finds_customer_by_first_name
-    sales_engine = "Placeholder"
-    merchant_repository = "Placeholder"
-    input_csv = fixture_path('customers_fixture.csv')
-    data = CustomerParser.new(input_csv, merchant_repository).load_csv
-    repo = CustomerRepository.new(data, sales_engine)
-
-    output_1 = repo.find_by_first_name("Joey")
-    output_2 = repo.find_by_first_name("joey")
-
-    assert_equal "Ondricka", output_1.last_name
-    assert_equal "Ondricka", output_2.last_name
+    result1 = customer_repository.find_by_first_name("Joey")
+    result2 = customer_repository.find_by_first_name("joey")
+    assert_equal 1, result1.id
+    assert_equal 1, result2.id
   end
 
   def test_it_finds_customer_by_last_name
-    sales_engine = "Placeholder"
-    merchant_repository = "Placeholder"
-    input_csv = fixture_path('customers_fixture.csv')
-    data = CustomerParser.new(input_csv, merchant_repository).load_csv
-    repo = CustomerRepository.new(data, sales_engine)
-
-    output_1 = repo.find_by_last_name("Reynolds")
-    output_2 = repo.find_by_last_name("reynolds")
-
-    assert_equal "Ramona", output_1.first_name
-    assert_equal "Ramona", output_2.first_name
+    result1 = customer_repository.find_by_last_name("Reynolds")
+    result2 = customer_repository.find_by_last_name("reynolds")
+    assert_equal "Ramona", result1.first_name
+    assert_equal "Ramona", result2.first_name
   end
 
   def test_it_finds_customer_by_created_at
-    sales_engine = "Placeholder"
-    merchant_repository = "Placeholder"
-    input_csv = fixture_path('customers_fixture.csv')
-    data = CustomerParser.new(input_csv, merchant_repository).load_csv
-    repo = CustomerRepository.new(data, sales_engine)
-
-    output = repo.find_by_created_at("2012-03-27 14:54:10 UTC")
-
-    assert_equal "Cecelia", output.first_name
+    result = customer_repository.find_by_created_at("2012-03-27 14:54:10 UTC")
+    assert_equal "Cecelia", result.first_name
   end
 
   def test_it_finds_customer_by_updated_at
-    sales_engine = "Placeholder"
-    merchant_repository = "Placeholder"
-    input_csv = fixture_path('customers_fixture.csv')
-    data = CustomerParser.new(input_csv, merchant_repository).load_csv
-    repo = CustomerRepository.new(data, sales_engine)
-
-    output = repo.find_by_updated_at("2012-03-27 14:54:10 UTC")
-
-    assert_equal "Cecelia", output.first_name
+    result = customer_repository.find_by_updated_at("2012-03-27 14:54:10 UTC")
+    assert_equal "Cecelia", result.first_name
   end
 
   def test_it_finds_all_customers_by_id
-    sales_engine = "Placeholder"
-    merchant_repository = "Placeholder"
-    input_csv = fixture_path('customers_fixture.csv')
-    data = CustomerParser.new(input_csv, merchant_repository).load_csv
-    repo = CustomerRepository.new(data, sales_engine)
-
-    output_1 = repo.find_all_by_id(1)
-    output_2 = repo.find_all_by_id(11)
-
-    assert_equal "Joey", output_1[0].first_name
-    assert_equal [], output_2
+    result1 = customer_repository.find_all_by_id(1)
+    result2 = customer_repository.find_all_by_id(11)
+    assert_equal Array, result1.class
+    assert_equal 1, result1.length
+    assert_equal 0, result2.length
   end
 
   def test_it_finds_all_customers_by_first_name
-    sales_engine = "Placeholder"
-    merchant_repository = "Placeholder"
-    input_csv = fixture_path('customers_fixture.csv')
-    data = CustomerParser.new(input_csv, merchant_repository).load_csv
-    repo = CustomerRepository.new(data, sales_engine)
-
-    output_1 = repo.find_all_by_first_name("Joey")
-    output_2 = repo.find_all_by_first_name("Sebastian")
-    output_3 = repo.find_all_by_first_name("joey")
-
-    assert_equal 1, output_1[0].id
-    assert_equal [], output_2
-    assert_equal 1, output_3[0].id
+    result1 = customer_repository.find_all_by_first_name("joey")
+    result2 = customer_repository.find_all_by_first_name("Sebastian")
+    result3 = customer_repository.find_all_by_first_name("Joey")
+    assert_equal Array, result1.class
+    assert_equal 1, result1.length
+    assert_equal 0, result2.length
+    assert_equal 1, result3.length
   end
 
   def test_it_finds_all_customers_by_last_name
-    sales_engine = "Placeholder"
-    merchant_repository = "Placeholder"
-    input_csv = fixture_path('customers_fixture.csv')
-    data = CustomerParser.new(input_csv, merchant_repository).load_csv
-    repo = CustomerRepository.new(data, sales_engine)
-
-    output_1 = repo.find_all_by_last_name("Osinski")
-    output_2 = repo.find_all_by_last_name("Abondano")
-    output_3 = repo.find_all_by_last_name("osinski")
-
-    assert_equal 2, output_1[0].id
-    assert_equal [], output_2
-    assert_equal 2, output_3[0].id
+    result1 = customer_repository.find_all_by_last_name("Osinski")
+    result2 = customer_repository.find_all_by_last_name("Abondano")
+    result3 = customer_repository.find_all_by_last_name("osinski")
+    assert_equal Array, result1.class
+    assert_equal 1, result1.length
+    assert_equal 0, result2.length
+    assert_equal 1, result3.length
   end
 
   def test_it_finds_all_customers_by_created_at
-    sales_engine = "Placeholder"
-    merchant_repository = "Placeholder"
-    input_csv = fixture_path('customers_fixture.csv')
-    data = CustomerParser.new(input_csv, merchant_repository).load_csv
-    repo = CustomerRepository.new(data, sales_engine)
-
-    output_1 = repo.find_all_by_created_at("2012-03-27 14:54:09 UTC")
-    output_2 = repo.find_all_by_created_at("2015-03-27 14:54:09 UTC")
-
-    assert_equal 1, output_1[0].id
-    assert_equal [], output_2
+    result1 = customer_repository.find_all_by_created_at("2012-03-27 14:54:09 UTC")
+    result2 = customer_repository.find_all_by_created_at("2015-03-27 14:54:09 UTC")
+    assert_equal Array, result1.class
+    assert_equal 1, result1.length
+    assert_equal 0, result2.length
   end
 
   def test_it_finds_all_customers_by_updated_at
-    sales_engine = "Placeholder"
-    merchant_repository = "Placeholder"
-    input_csv = fixture_path('customers_fixture.csv')
-    data = CustomerParser.new(input_csv, merchant_repository).load_csv
-    repo = CustomerRepository.new(data, sales_engine)
-
-    output_1 = repo.find_all_by_updated_at("2012-03-27 14:54:09 UTC")
-    output_2 = repo.find_all_by_updated_at("2015-03-27 14:54:09 UTC")
-
-    assert_equal 1, output_1[0].id
-    assert_equal [], output_2
+    result1 = customer_repository.find_all_by_updated_at("2012-03-27 14:54:09 UTC")
+    result2 = customer_repository.find_all_by_updated_at("2015-03-27 14:54:09 UTC")
+    assert_equal Array, result1.class
+    assert_equal 1, result1.length
+    assert_equal 0, result2.length
   end
 
   private
-
-  def fixture_path(file_name)
-    File.join(fixtures_directory, file_name)
-  end
 
   def fixtures_directory
     File.expand_path('../../data/fixtures', __FILE__)
