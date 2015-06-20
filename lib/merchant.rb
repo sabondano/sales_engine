099e74e969202178d8eff76a388a89e367581503
Sebastian Abondano <sabondano1@gmail.com>
2015-06-20 16:08:03 -0600

    add revenue method for merchant class (business intelligence)

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -22,4 +22,17 @@ class Merchant
     @invoices ||= @repository.find_invoices(id)
   end
 
+  def revenue
+    successful_invoice_items =  successful_invoices.map { |i| i.invoice_items }.flatten
+    successful_invoice_items.reduce(0) do |revenue, invoice_item|
+      revenue + (invoice_item.quantity * invoice_item.unit_price)
+    end
+  end
+
+  def successful_invoices
+    successful_transactions ||= invoices.map { |i| i.transactions }.flatten
+    successful_transactions.select! { |t| t.result == 'success'}
+    invoices = successful_transactions.map { |t| t.invoice }
+    invoices
+  end
 end
